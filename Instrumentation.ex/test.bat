58967db45895697eaacbef11265b2fdd61993972
eichelberger <eichelberger@sse.uni-hildesheim.de>
2016-08-23 17:47:02 +0200

    deploying the full gearsBridge/locutor

--- Instrumentation.ex/test.bat
+++ Instrumentation.ex/test.bat
@@ -1,17 +1,20 @@
 @echo off
 set CP_BIN=bin
 set CP_TESTS=dist\tests.jar
+if not exist generated mkdir generated
 REM RUN ant jar-testAgent before
 
 REM bypass individual tests 
-GOTO all
+REM GOTO all
 REM check need for -rt.jar
 
-set TEST=SystemExitTest
+REM set TEST=InstanceIdentifierTest
+set TEST=MultiRecIdTest
 REM java -classpath dist/win32/spass-meter-ant.jar de.uni_hildesheim.sse.monitoring.runtime.preprocess.Preprocess dist/tests.jar generated/tests-static.jar logLevel=SEVERE,overhead=false,configDetect=false,localEventProcessing=true,out=generated/test.log
 REM java -javaagent:dist/win32/spass-meter-ia.jar=logLevel=SEVERE,overhead=false,configDetect=false,localEventProcessing=true,out=generated/test.log -Dbla=x -classpath %CP_TESTS% test.%TEST%
 REM java -javaagent:dist/win32/spass-meter-ia.jar=logLevel=SEVERE,overhead=false,configDetect=false,localEventProcessing=true,groupAccounting=INDIRECT,out=generated/test.log,iFactory=de.uni_hildesheim.sse.monitoring.runtime.instrumentation.asmTree.Factory -Dbla=x -Dindirect=true -classpath %CP_TESTS% test.%TEST%
-java -javaagent:dist/win32/spass-meter-ia.jar=logLevel=SEVERE,overhead=false,configDetect=false,localEventProcessing=true,out=generated/test.log,xmlconfig=src/test/%TEST%.xml -Dbla=x -classpath %CP_TESTS% test.%TEST%
+REM java -javaagent:dist/win32/spass-meter-ia.jar=logLevel=SEVERE,overhead=false,configDetect=false,localEventProcessing=true,out=generated/test.log,xmlconfig=src/test/%TEST%.xml -Dbla=x -classpath %CP_TESTS% test.%TEST%
+java -javaagent:dist/win32/spass-meter-ia.jar=logLevel=SEVERE,overhead=false,configDetect=false,localEventProcessing=true,out=generated/test.log,iFactory=de.uni_hildesheim.sse.monitoring.runtime.instrumentation.asmTree.Factory,xmlconfig=src/test/%TEST%.xml -Dbla=x -classpath %CP_TESTS% test.%TEST%
 REM java -javaagent:dist/win32/spass-meter-ia.jar=logLevel=SEVERE,overhead=false,configDetect=false,localEventProcessing=true,groupAccounting=INDIRECT,out=generated/test.log,xmlconfig=src/test/%TEST%.xml -Dbla=x -Dindirect=true -classpath %CP_TESTS% test.%TEST%
 REM java -javaagent:dist/win32/spass-meter-ia.jar=logLevel=SEVERE,overhead=false,configDetect=false,localEventProcessing=true,out=generated/test.log,xmlconfig=src/test/%TEST%-native.xml -Dbla=x -classpath %CP_TESTS% test.%TEST%
 
