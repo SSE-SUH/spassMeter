e893bcb39feeca95b3641a54168bf629bdb6c5fc
eichelberger <eichelberger@sse.uni-hildesheim.de>
2017-11-27 14:52:35 +0100

    missing maven central requirements, fixing central deployment script

--- Instrumentation.ex/deployCentral.bat
+++ Instrumentation.ex/deployCentral.bat
@@ -16,6 +16,7 @@ SET TARGET=https://oss.sonatype.org/service/local/staging/deploy/maven2
 SET REPO=ossrh
 SET DEPLOYCMD=mvn gpg:sign-and-deploy-file -Durl=%TARGET% -DrepositoryId=%REPO%
 
+REM deploy the individual artifacts for SPASS-meter
 call :DeployArtifact spass-meter %SPASS_VERSION%
 call :DeployArtifact spass-meter-annotations %SPASS_VERSION%
 call :DeployArtifact spass-meter-ant %SPASS_VERSION%
@@ -25,8 +26,12 @@ call :DeployArtifact spass-meter-rt %SPASS_VERSION%
 call :DeployArtifact spass-meter-static %SPASS_VERSION%
 call :DeployArtifact locutor %LOCUTOR_VERSION%
 
+REM done here, don't go into "sub-program"
 goto :end
 
+REM deploy a certain artifact (including classified artifacts)
+REM param1: name/id of the artifact
+REM param2: version of the artifact to deploy
 :DeployArtifact
     setlocal
 	SET ARTIFACTNAME=%1
@@ -38,13 +43,15 @@ goto :end
 	SET JAVADOC=%ARTIFACTPREFIX%-javadoc.jar
     SET URLPREFIX=%LOCALREPO%/%ARTIFACTNAME%/%ARTIFACTVERSION%
 
-	echo wget %URLPREFIX%/%POM% -O %DIR%\%POM%
-	echo wget %URLPREFIX%/%JAR% -O %DIR%\%JAR%
-	echo wget %URLPREFIX%/%SOURCES% -O %DIR%\%SOURCES%
-	echo wget %URLPREFIX%/%JAVADOC% -O %DIR%\%JAVADOC%
-	echo %DEPLOYCMD% -DpomFile=%DIR%\%POM% -Dfile=%DIR%\%JAR%
-	echo %DEPLOYCMD% -DpomFile=%DIR%\%POM% -Dfile=%DIR%\%SOURCES% -Dclassifier=sources
-	echo %DEPLOYCMD% -DpomFile=%DIR%\%POM% -Dfile=%DIR%\%JAVADOC% -Dclassifier=javadoc
+	REM download relevant physical artifacts
+	wget %URLPREFIX%/%POM% -O %DIR%\%POM%
+	wget %URLPREFIX%/%JAR% -O %DIR%\%JAR%
+	wget %URLPREFIX%/%SOURCES% -O %DIR%\%SOURCES%
+	wget %URLPREFIX%/%JAVADOC% -O %DIR%\%JAVADOC%
+	REM deploy jar, sources, docs via POM to central
+	call %DEPLOYCMD% -DpomFile=%DIR%\%POM% -Dfile=%DIR%\%JAR%
+	call %DEPLOYCMD% -DpomFile=%DIR%\%POM% -Dfile=%DIR%\%SOURCES% -Dclassifier=sources
+	call %DEPLOYCMD% -DpomFile=%DIR%\%POM% -Dfile=%DIR%\%JAVADOC% -Dclassifier=javadoc
 
 	endlocal
 	goto :eof
