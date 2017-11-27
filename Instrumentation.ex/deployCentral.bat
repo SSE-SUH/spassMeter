d55a7b4e13f4e9365f167f2f6863b3d9df8659e0
eichelberger <eichelberger@sse.uni-hildesheim.de>
2017-11-27 15:04:03 +0100

    relase 1.23 (maven central)

--- Instrumentation.ex/deployCentral.bat
+++ Instrumentation.ex/deployCentral.bat
@@ -9,8 +9,8 @@ REM run this script
 REM goto https://oss.sonatype.org/#welcome, staging repositories, deuni-hildesheim...*, close for check/deploy
 
 SET LOCALREPO=http://projects.sse.uni-hildesheim.de/qm/maven/de/uni-hildesheim/sse/spassMeter
-SET SPASS_VERSION=1.27
-SET LOCUTOR_VERSION=1.11
+SET SPASS_VERSION=1.28
+SET LOCUTOR_VERSION=1.12
 SET DIR=.
 SET TARGET=https://oss.sonatype.org/service/local/staging/deploy/maven2
 SET REPO=ossrh
